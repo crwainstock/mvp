7a3a553f6362fc640b0cfadaa060941a0b61164f
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-16 20:07:40 +0100

    delete some stuff from client, add icon

--- my-express-app/routes/api.js
+++ my-express-app/routes/api.js
@@ -1,5 +1,8 @@
+//THIS FILE DEFINES THE ROUTER FUNCTIONS TO ACCESS DATA FROM THE DATABASE ON THE SERVERSIDE.
+
 var express = require("express");
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
