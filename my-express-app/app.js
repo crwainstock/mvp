6bcdf989e00b57c53a295108c1dba0b90052ddb3
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-17 09:03:47 +0100

    added getItems async function to index.js file -- do I need to keep the public file that's returning index.html on the BE?

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", indexRouter); //This is the base URL
 app.use("/users", usersRouter);
 
 module.exports = app;
--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -2,11 +2,21 @@
 
 var express = require("express");
 var router = express.Router();
-const db = require("../model/helper");
+const db = require("../model/helper"); //So this file can access the helper functions.
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send({ title: "Express" });
+  res.send("Welcome to My Library API"); //This is returning something else? The index.html in the public folder for Express
 });
+
+const getItems = async (req, res) => {
+  try {
+    const result = await db("SELECT * FROM mylibrary");
+    const items = result.data;
+    res.send(items);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 module.exports = router;
