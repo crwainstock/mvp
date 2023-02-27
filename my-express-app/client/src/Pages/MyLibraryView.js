78a749cd33f1082fd37a191a892610a9dae78c06
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-27 19:41:38 +0100

    appears id is empty for target, so e.target.id returns , which means functions return undefined. How can I target the key for the book div here? ...

--- my-express-app/client/src/Pages/MyLibraryView.js
+++ my-express-app/client/src/Pages/MyLibraryView.js
@@ -64,6 +64,7 @@ function MyLibraryView() {
   const fetchDBBooks = async (bookId) => {
     setLoading(true);
     try {
+      console.log(bookId);
       //Get all database books
       let results = await fetch(`/mylibrary`);
       let data = await results.json();
@@ -137,24 +138,17 @@ function MyLibraryView() {
                       See More
                     </button>
                   </Link>
-                  <div
-                    id="deleteIcon"
-                    className="col"
-                    onClick={(e) => {
-                      deleteBook(e.target.id);
-                    }}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      className="bi bi-x-circle"
-                      viewBox="0 0 16 16"
+                  <div id="deleteIcon" className="col" key={book.id}>
+                    <button
+                      className="rounded btn btn-danger"
+                      onClick={(e) => {
+                        deleteBook(e);
+                        console.log(e); // Returning those path values below...???
+                      }}
                     >
-                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                    </svg>
+                      {" "}
+                      Delete Book{" "}
+                    </button>
                   </div>
                 </div>
               </div>
