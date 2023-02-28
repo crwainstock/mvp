292a23bee62aac4055e713acd3341a370a5fcc24
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-28 13:54:42 +0100

    final touches

--- my-express-app/client/src/Pages/Home.js
+++ my-express-app/client/src/Pages/Home.js
@@ -1,18 +1,13 @@
 import React from "react";
 import Search from "../Components/Search";
 import MyLibraryWidget from "../Components/MyLibraryWidget";
-import { useState } from "react";
 
 function Home() {
-  const handleSearch = () => {
-    //idk know what I need here.
-  };
-
   return (
     <div className="App">
-      <h1>Kid Library</h1>
+      <h1>My Library App</h1>
       <MyLibraryWidget />
-      <Search searchResultsCB={handleSearch} />
+      <Search />
     </div>
   );
 }
