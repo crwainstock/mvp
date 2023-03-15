74ccd905558c6ab8314d57d75dfd94e276d3b33c
Crystal <crwainstock@gmail.com>
2023-03-15 10:32:32 +0100

    adding useContext to project, just started, not complete

--- my-express-app/client/src/App.js
+++ my-express-app/client/src/App.js
@@ -1,17 +1,23 @@
-import Home from "./Pages/Home";
+import { useContext } from "react";
 
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import MyLibraryView from "./Pages/MyLibraryView";
 import BookDetailView from "./Pages/BookDetailView";
+import { MyLibraryContext } from "./MyLibraryContext";
 
 function App() {
+  // Define data object with state variable & function to modify variable
+  // let myLibraryObj = {}
   return (
     <BrowserRouter>
       <Routes>
+        {/* <MyLibraryContext.Provider value={}> */}
         <Route path="/" element={<Home />} />
         <Route path="/myLibrary" element={<MyLibraryView />} />
         <Route path="/myLibrary/:id" element={<BookDetailView />} />
+        {/* </MyLibraryContext.Provider> */}
       </Routes>
     </BrowserRouter>
   );
